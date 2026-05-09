41548bf71b96ef7add9fde39b0a56ab5c40e5def
Jumana Ayoub <ayoub.jumana1@gmail.com>
2026-05-09 04:14:00 -0700

    Update file_manager.h

--- file_manager.h
+++ file_manager.h
@@ -1,25 +1,15 @@
 #ifndef FILE_MANAGER_H
 #define FILE_MANAGER_H
 
-#define MAX_FILES 100
-#define MAX_NAME 100
-#define MAX_CONTENT 1000
-
-typedef struct {
-    char name[MAX_NAME];
-    char content[MAX_CONTENT];
-    int isOpen;
-} File;
-
-extern File files[MAX_FILES];
-extern int fileCount;
-
-void createFile(char name[]);
-void openFile(char name[]);
-void closeFile(char name[]);
-void writeToFile(char name[], char text[]);
-void readFile(char name[]);
+// Main file operation functions
+void createFile();
+void writeToFile();
+void readFile();
 void listFiles();
-int searchFile(char name[]);
+void showFileDetails();
+void deleteFile();
+
+// Search function
+void searchFile();
 
 #endif
